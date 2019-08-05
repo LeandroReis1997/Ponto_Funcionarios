08382a78af3d836ed130cdecfb4b17bd4969e30f
Leandro Reis <leandro.visualc@gmail.com>
2019-08-05 20:03:41 -0300

    Tratando campos input

---
+++ PointRecord/PointRecord/wwwroot/js/mask.js
@@ -0,0 +1,6 @@
+	$(document).ready(function () {
+        $("#tel").mask('(00) 0000-0000');
+        $("#date").mask("99/99/9999");
+        $("#cpf").mask("999.999.999-99");
+        jQuery("#cep").mask("99999-999");
+		});
