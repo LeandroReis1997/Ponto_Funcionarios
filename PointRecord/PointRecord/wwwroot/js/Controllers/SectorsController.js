a22645b5406944eb0009b560d8c0952d35e7fa48
Leandro <leandro.visualc@gmail.com>
2019-08-15 14:35:39 -0300

    Adicionando aquivo js sector

---
+++ PointRecord/PointRecord/wwwroot/js/Controllers/SectorsController.js
@@ -0,0 +1,60 @@
+﻿class SectorsController {
+    StartSector() {
+        $(".btnRemover").on("click", function () {
+            var id = $(this).attr("id");
+            let url = "/sectors/delete/" + id;
+            var name = $(this).attr("name");
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: true
+            })
+
+            swalWithBootstrapButtons.fire({
+                title: 'Você tem certeza?',
+                text: `Deseja apagar o setor ${name} ?`,
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sim, Confirmar',
+                cancelButtonText: 'Não, Cancelar',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.value) {
+                    setTimeout(() => {
+                        $(this).parent().parent().parent().fadeOut();
+                    }, 800);
+                    setTimeout(() => {
+                        msg.hide();
+                    }, 5000);
+                    swalWithBootstrapButtons.fire(
+                        'Deletado!',
+                        `O setor ${name} foi removido`,
+                        'success')
+
+                    $.ajax({
+                        type: 'POST',
+                        url: url
+
+                    }).fail((error) => {
+                        let msg = $("#msg");
+                        msg.text("Não possível completar a operação");
+                        msg.show();
+                        setTimeout(() => {
+                            msg.hide();
+                        }, 5000);
+                    });
+                } else if (
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    swalWithBootstrapButtons.fire(
+                        'Cancelado',
+                        'Exclusão abortada!',
+                        'error'
+                    )
+                }
+            })
+        });
+    }
+}
